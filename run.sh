5b72f0c3f03b9e87bb8146d64fce65d38c6ee239
dmpe <cincenko@outlook.com>
2023-01-07 12:40:48 +0100

    extract more from ansible palo alto: url category and

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 base="$(dirname $(readlink -f "$BASH_SOURCE[0]"))"
 
-docker run -v $(pwd):/home -it ansible:latest -- bash
+docker run -v $(pwd):/home -it ansible:latest bash
 
-ansible-playbook -i $base/ansible/hosts_vars/inventory $base/ansible/fetch-firewall-rules.yml
+ansible-playbook ansible/fetch-firewall-rules.yml
 
